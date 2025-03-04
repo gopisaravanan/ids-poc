b73954830bfb93822664f6742b55d6fb2de852bd
Gopi saravanan R <gopi.saravanan@kaaylabs.com>
2025-03-04 19:50:23 +0530

    bg color change for version test

--- packages/button/src/Button.tsx
+++ packages/button/src/Button.tsx
@@ -11,7 +11,7 @@ const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
       onClick={onClick}
       style={{
         padding: "8px 16px",
-        backgroundColor: "#007bff",
+        backgroundColor: "#388e3c",
         color: "#fff",
         border: "none",
         borderRadius: "4px",
