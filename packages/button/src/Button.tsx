237db784788ea31e69ac27c10982828d5cf4f8bd
Gopi saravanan R <gopi.saravanan@kaaylabs.com>
2025-03-07 11:54:23 +0530

    button background color change

--- packages/button/src/Button.tsx
+++ packages/button/src/Button.tsx
@@ -9,7 +9,7 @@ export interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
   return (
     <button
-    className="px-4 py-2 bg-pink-600 text-white border-none rounded-md cursor-pointer"
+    className="px-4 py-2 bg-violet-600 text-white border-none rounded-md cursor-pointer"
     onClick={onClick}
     >
       {children}
